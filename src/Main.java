fa7fda3e524992ea2bf26ff4a300cb8d7e7a2b07
JSQdev <148073798+devJSQ@users.noreply.github.com>
2024-07-30 17:35:16 +0300

    Update Main.java

--- src/Main.java
+++ src/Main.java
@@ -6,7 +6,14 @@ public class Main {
         graph.addVertex(new Vertex('B'));
         graph.addVertex(new Vertex('C'));
 
+        graph.addEdge(0, 1); // A -> B
+        graph.addEdge(1, 2); // B -> C
+        graph.addEdge(2, 0); // C -> A
+
         graph.display();
+    }
+}
+
 
     }
 }
